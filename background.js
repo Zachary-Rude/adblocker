e697ba261b097e9a00cc2348c4f8700ec237c4a3
Zachary-Rude <84681078+Zachary-Rude@users.noreply.github.com>
2021-09-17 16:10:07 -0700

    Update background.js

--- background.js
+++ background.js
@@ -19,14 +19,3 @@ chrome.runtime.onInstalled.addListener(details => {
     chrome.runtime.setUninstallURL('https://ad-blocker.zacharyrude.repl.co/uninstall.html');
   }
 });
-
-
-chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-  chrome.tabs.executeScript(
-    tabs[0].id,
-    { code: `let ads = document.querySelectorAll("[class*=ad], [class*=doubleclick], [class*=Banner], [class*=banner], [class*=Ad], [id*=ad], [id*=doubleclick], [id*=Banner], [id*=banner], [id*=Ad]");
-ads.forEach(ad => {
-  ad.style.display = "none !important";
-});` }
-  );
-});
